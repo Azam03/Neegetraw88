7936bf908de87de4e396ff89b7c98daf55de68fb
Azam03 <56317949+Azam03@users.noreply.github.com>
2019-10-09 13:17:12 +0700

    Add files via upload

---
+++ Getraw.sh
@@ -0,0 +1,21 @@
+#!/System/bin/sh
+
+""
+Welcome To Getraw
+mmm  mmmmmmmmmmmmm mmmmm    mm  m     m        mmmmm   mmmm           mmm
+ m"   " #        #    #   "#   ##  #  #  #          #    #"   "        m"   "
+ #   mm #mmmmm   #    #mmmm"  #  # " #"# #          #    "#mmm         #
+ #    # #        #    #   "m  #mm#  ## ##"          #        "#        #
+  "mmm" #mmmmm   #    #    " #    # #   #         mm#mm  "mmm#"         "mmm"
+
+
+
+
+
+
+  mmmm  m    m mmmmmm mmmmm    mm     mmm  m    m
+ m"  "m ##  ## #      #    #   ##   m"   " #  m"
+ #    # # ## # #mmmmm #mmmm"  #  #  #      #m#
+ #    # # "" # #      #    #  #mm#  #      #  #m
+  #mm#  #    # #mmmmm #mmmm" #    #  "mmm" #   "m
+
